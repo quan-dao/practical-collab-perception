b2e102c6dc7c137aad08844ebdf48f84a0ee0391
Minh-Quan DAO <minh-quan.dao@ls2n.fr>
2022-07-28 17:15:56 +0200

    Add viz of nuscenes_dataset

---
+++ _dev_space/viz_nuscenes_dataset.py
@@ -0,0 +1,78 @@
+import numpy as np
+from pcdet.config import cfg, cfg_from_yaml_file
+from pcdet.utils import common_utils
+from pcdet.datasets import NuScenesDataset
+from get_clean_pointcloud import show_pointcloud, apply_transform_to_points
+import matplotlib.pyplot as plt
+
+
+def viz_boxes(boxes: np.ndarray):
+    xs = np.array([1, 1, 1, 1, -1, -1, -1, -1], dtype=float) / 2.0
+    ys = np.array([-1, 1, 1, -1, -1, 1, 1, -1], dtype=float) / 2.0
+    zs = np.array([1, 1, -1, -1, 1, 1, -1, -1], dtype=float) / 2.0
+    out = []
+    for i in range(boxes.shape[0]):
+        box = boxes[i]
+        dx, dy, dz = box[3: 6].tolist()
+        vers = np.concatenate([xs.reshape(-1, 1) * dx, ys.reshape(-1, 1) * dy, zs.reshape(-1, 1) * dz], axis=1)  # (8, 3)
+        ref_from_box = np.eye(4)
+        yaw = box[6]
+        cy, sy = np.cos(yaw), np.sin(yaw)
+        ref_from_box[:3, :3] = np.array([
+            [cy, -sy, 0],
+            [sy, cy, 0],
+            [0, 0, 1]
+        ])
+        ref_from_box[:3, 3] = box[:3]
+        vers = apply_transform_to_points(ref_from_box, vers)
+        out.append(vers)
+    return out
+
+
+np.random.seed(666)
+cfg_file = '../tools/cfgs/dataset_configs/nuscenes_dataset.yaml'
+cfg_from_yaml_file(cfg_file, cfg)
+
+logger = common_utils.create_logger('./dummy_log.txt')
+
+cfg.CLASS_NAMES = ['car', 'truck', 'construction_vehicle', 'bus', 'trailer',
+                   'barrier', 'motorcycle', 'bicycle', 'pedestrian', 'traffic_cone']
+# cfg.DATA_AUGMENTOR.DISABLE_AUG_LIST = ['random_world_flip', 'random_world_rotation',
+#                                        'random_world_scaling']
+
+
+nuscenes_dataset = NuScenesDataset(cfg, cfg.CLASS_NAMES, training=True, logger=logger)
+data_dict = nuscenes_dataset[10]
+
+
+for k, v in data_dict.items():
+    out = f"{k} | {type(v)} | "
+    if isinstance(v, str):
+        out += v
+    elif isinstance(v, np.ndarray):
+        out += f"{v.shape}"
+    elif isinstance(v, float):
+        out += f"{v}"
+    elif isinstance(v, np.bool_):
+        out += f"{v.item()}"
+    print(out)
+
+print(data_dict['metadata'])
+
+# 10: f2d48fe35d5646659dc339e1d36886f9
+sample_rec = nuscenes_dataset.nusc.get('sample', data_dict['metadata']['token'])
+nuscenes_dataset.nusc.render_sample_data(sample_rec['data']['CAM_FRONT'])
+plt.show()
+
+pc = data_dict['points']
+gt_boxes = data_dict['gt_boxes']
+print(gt_boxes[:10, -1])
+
+_boxes = viz_boxes(gt_boxes)
+show_pointcloud(pc[:, :3], _boxes)
+
+
+
+
+
+
