7a631e890fa9400bfd6ea3c245a3808d2bee7da1
Minh-Quan DAO <minh-quan.dao@ls2n.fr>
2022-08-04 17:21:31 +0200

    Add kd affinity loss (from MonoDistill)

--- _dev_space/distill_bev.py
+++ _dev_space/distill_bev.py
@@ -15,6 +15,7 @@ class KnowledgeDistillationLoss(Detector3DTemplate):
             param.requires_grad = False
         # loss
         self.kl_loss = nn.KLDivLoss(reduction="batchmean")
+        self.aff_cfg = self.model_cfg.KD_AFFINITY_LOSS
 
     def load_teacher_weights(self):
         filename = self.model_cfg.TEACHER_CKPT
@@ -22,7 +23,7 @@ class KnowledgeDistillationLoss(Detector3DTemplate):
         model_state_disk = checkpoint['model_state']
         state_dict, update_model_state = self._load_state_dict(model_state_disk, strict=False)
 
-    def forward(self, batch_dict):
+    def forward(self, batch_dict, tb_dict):
         # forward prop in teacher to get teacher's encoder_feat_map
         teacher_dict = {'points': batch_dict['points']}
         for cur_module in self.module_list:
@@ -31,8 +32,18 @@ class KnowledgeDistillationLoss(Detector3DTemplate):
         teacher_encoder_fmap = teacher_dict['spatial_features_2d']  # (B, C, H, W)
         student_encoder_fmap = batch_dict['spatial_features_2d']  # (B, C, H, W)
         assert teacher_encoder_fmap.shape == student_encoder_fmap.shape
-        kd_loss = self._kd_loss_KLdiv(student_encoder_fmap, teacher_encoder_fmap) * self.model_cfg.LOSS_WEIGHTS[0]
-        return kd_loss
+
+        kl_div = self._kd_loss_KLdiv(student_encoder_fmap, teacher_encoder_fmap) * self.model_cfg.LOSS_WEIGHTS[0]
+        aff_loss = self._kd_loss_affinity(
+            student_encoder_fmap, teacher_encoder_fmap,
+            self.aff_cfg.NORMALIZE_USING_NORM, self.aff_cfg.DOWNSAMPLE_FACTOR
+        ) * self.model_cfg.LOSS_WEIGHTS[1]
+
+        kd_loss = kl_div + aff_loss
+        tb_dict['kd_loss'] = kd_loss.item()
+        tb_dict['kd_kl_div'] = kl_div.item()
+        tb_dict['kd_aff_loss'] = aff_loss.item()
+        return kd_loss, tb_dict
 
     def _kd_loss_KLdiv(self, student_fmap, teacher_fmap):
         n_bev_channels = student_fmap.shape[1]
@@ -41,3 +52,29 @@ class KnowledgeDistillationLoss(Detector3DTemplate):
         kl_div = self.kl_loss(F.log_softmax(student_fmap, dim=1), F.softmax(teacher_fmap, dim=1))
         return kl_div
 
+    @staticmethod
+    def _kd_loss_affinity(student_fmap, teacher_fmap, normalize_using_norm=True, downsample_factor=1):
+        def _compute_affinity(x, _normalize_using_norm):
+            assert len(x.shape) == 4, f"input must have shape of (B, C, H, W), current shape: {x.shape}"
+            batch_size, n_channels = x.shape[:2]
+            x = x.view(batch_size, n_channels, -1)  # (B, C, N) | N = H * W
+            aff = torch.bmm(x.transpose(1, 2).contiguous(), x)  # (B, N, N)
+            if _normalize_using_norm:
+                norm = torch.linalg.norm(x, dim=1, keepdim=True)  # (B, 1, N)
+                normalizer = torch.bmm(norm.permute(1, 2).contiguous(), norm)  # (B, N, N)
+                mask_valid_norm = normalizer > 1e-4  # (B, N, N)
+                aff[mask_valid_norm] = aff[mask_valid_norm] / normalizer[mask_valid_norm]
+            else:
+                mask_valid_norm = torch.ones_like(aff, dtype=torch.bool)
+            return aff, mask_valid_norm
+
+        if downsample_factor > 1:
+            scale_factor = 1.0 / float(downsample_factor)
+            student_fmap = F.interpolate(student_fmap, scale_factor=scale_factor, mode='bilinear')
+            teacher_fmap = F.interpolate(teacher_fmap, scale_factor=scale_factor, mode='bilinear')
+
+        student_aff, stud_valid = _compute_affinity(student_fmap, normalize_using_norm)  # (B, N, N) | N = H * W
+        teacher_aff, tea_valid = _compute_affinity(teacher_fmap, normalize_using_norm)  # (B, N, N) | N = H * W
+        _valid = torch.logical_and(stud_valid, tea_valid)  # (B, N, N)
+        aff_loss = F.l1_loss(student_aff[_valid], teacher_aff[_valid], reduction='mean')
+        return aff_loss
--- pcdet/models/detectors/distill_bev_centerpoint.py
+++ pcdet/models/detectors/distill_bev_centerpoint.py
@@ -16,13 +16,12 @@ class DistillBEVCenterPoint(CenterPoint):
 
         if self.training:
             loss, tb_dict, disp_dict = self.get_training_loss()
-            kd_loss = self.kd_loss_module(batch_dict)
+            kd_loss, tb_dict = self.kd_loss_module(batch_dict, tb_dict)
             loss = loss + kd_loss
 
             ret_dict = {
                 'loss': loss,
             }
-            tb_dict['kd_loss'] = kd_loss
             return ret_dict, tb_dict, disp_dict
         else:
             pred_dicts, recall_dicts = self.post_processing(batch_dict)
