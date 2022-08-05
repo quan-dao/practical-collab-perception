633bd45c2562ad1685a94019280d7b4a5ae3c922
Minh-Quan DAO <minh-quan.dao@ls2n.fr>
2022-08-05 11:40:42 +0200

    Skip generating pred boxes in Teacher's dense_head

--- pcdet/models/dense_heads/center_head.py
+++ pcdet/models/dense_heads/center_head.py
@@ -338,7 +338,10 @@ class CenterHead(nn.Module):
             self.forward_ret_dict['target_dicts'] = target_dict
 
         self.forward_ret_dict['pred_dicts'] = pred_dicts
+
         data_dict['pred_heatmaps'] = [_pred_dict['hm'] for _pred_dict in pred_dicts]
+        if self.model_cfg.get('IS_TEACHING', False):
+            return data_dict  # early stop in case of the head belong to Teacher
 
         if not self.training or self.predict_boxes_when_training:
             pred_dicts = self.generate_predicted_boxes(
