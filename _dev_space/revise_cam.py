a6009141b9c8347e0b21f06a0899bebe9ce83635
Minh-Quan DAO <minh-quan.dao@ls2n.fr>
2022-08-31 14:49:40 +0200

    missing method for building network

--- _dev_space/revise_cam.py
+++ _dev_space/revise_cam.py
@@ -5,6 +5,7 @@ from easydict import EasyDict as edict
 from pcdet.models.backbones_3d import vfe
 from pcdet.models.backbones_2d import map_to_bev
 from pcdet.models import backbones_2d
+from pcdet.utils.spconv_utils import find_all_spconv_keys
 
 from sklearn.cluster import DBSCAN
 from _dev_space.bev_segmentation_utils import sigmoid
@@ -365,3 +366,34 @@ class PointCloudCorrector(nn.Module):
         model_info_dict['module_list'].append(backbone_2d_module)
         model_info_dict['num_bev_features'] = backbone_2d_module.num_bev_features
         return backbone_2d_module, model_info_dict
+
+    def _load_state_dict(self, model_state_disk, *, strict=True):
+        state_dict = self.state_dict()  # local cache of state_dict
+
+        spconv_keys = find_all_spconv_keys(self)
+
+        update_model_state = {}
+        for key, val in model_state_disk.items():
+            if key in spconv_keys and key in state_dict and state_dict[key].shape != val.shape:
+                # with different spconv versions, we need to adapt weight shapes for spconv blocks
+                # adapt spconv weights from version 1.x to version 2.x if you used weights from spconv 1.x
+
+                val_native = val.transpose(-1, -2)  # (k1, k2, k3, c_in, c_out) to (k1, k2, k3, c_out, c_in)
+                if val_native.shape == state_dict[key].shape:
+                    val = val_native.contiguous()
+                else:
+                    assert val.shape.__len__() == 5, 'currently only spconv 3D is supported'
+                    val_implicit = val.permute(4, 0, 1, 2, 3)  # (k1, k2, k3, c_in, c_out) to (c_out, k1, k2, k3, c_in)
+                    if val_implicit.shape == state_dict[key].shape:
+                        val = val_implicit.contiguous()
+
+            if key in state_dict and state_dict[key].shape == val.shape:
+                update_model_state[key] = val
+                # logger.info('Update weight %s: %s' % (key, str(val.shape)))
+
+        if strict:
+            self.load_state_dict(update_model_state)
+        else:
+            state_dict.update(update_model_state)
+            self.load_state_dict(state_dict)
+        return state_dict, update_model_state
