e0d7fbbce47fcbd271edd4b779a60798e09e09c5
Minh-Quan DAO <minh-quan.dao@ls2n.fr>
2022-10-18 14:07:48 +0200

    add correcting points

--- _dev_space/_test/_test_get_sweeps_instance_centric.py
+++ _dev_space/_test/_test_get_sweeps_instance_centric.py
@@ -1,5 +1,5 @@
 from nuscenes.nuscenes import NuScenes
-from _dev_space.tools_box import show_pointcloud, get_nuscenes_sensor_pose_in_global, tf
+from _dev_space.tools_box import show_pointcloud, get_nuscenes_sensor_pose_in_global, tf, apply_tf
 from _dev_space.get_sweeps_instance_centric import inst_centric_get_sweeps
 from _dev_space.viz_tools import viz_boxes
 import matplotlib.pyplot as plt
@@ -42,7 +42,30 @@ print(f"_boxes: {_boxes.shape}")
 _boxes = viz_boxes(_boxes)
 show_pointcloud(points[:, :3], _boxes, fgr_mask=points[:, -1] > -1)
 
-# TODO: test correcting point cloud with 'instances'
+# test correcting point cloud with 'instances'
+points_instance_idx = points[:, -1].astype(int)
+bg_points = points[points_instance_idx == -1]
+fg_points = points[points_instance_idx > -1]
 
+unq_instance_indices = np.unique(points_instance_idx)
+
+instances = out['instances']
+instances_sweep_indices = out['instances_sweep_indices']
+
+for inst_idx in unq_instance_indices:
+    inst_mask = fg_points[:, -1].astype(int) == inst_idx
+    inst_poses = instances[inst_idx]  # list
+    inst_sid = instances_sweep_indices[inst_idx]  # list
+
+    points_sid = fg_points[inst_mask, -2].astype(int)
+    unq_points_sid = np.unique(points_sid)
+    for sid in unq_points_sid:
+        mask = inst_mask & (fg_points[:, -2].astype(int) == sid)
+        t_ = inst_poses[-1] @ np.linalg.inv(inst_poses[inst_sid.index(sid)])
+        fg_points[mask, :3] = apply_tf(t_, fg_points[mask, :3])
+
+
+_points = np.concatenate([bg_points, fg_points], axis=0)
+show_pointcloud(_points[:, :3], _boxes, fgr_mask=_points[:, -1] > -1)
 
 
