95a1a51556a4d6c835a91cc3bdf423718e3b61cd
Minh-Quan DAO <minh-quan.dao@ls2n.fr>
2022-10-21 17:02:40 +0200

    ready for training

--- pcdet/models/detectors/__init__.py
+++ pcdet/models/detectors/__init__.py
@@ -11,6 +11,7 @@ from .centerpoint import CenterPoint
 from .pv_rcnn_plusplus import PVRCNNPlusPlus
 from .distill_bev_centerpoint import DistillBEVCenterPoint
 from .bev_segementation import BEVSegmentation
+from .alginer import Aligner
 
 __all__ = {
     'Detector3DTemplate': Detector3DTemplate,
@@ -25,7 +26,8 @@ __all__ = {
     'CenterPoint': CenterPoint,
     'PVRCNNPlusPlus': PVRCNNPlusPlus,
     'DistillBEVCenterPoint': DistillBEVCenterPoint,
-    'BEVSegmentation': BEVSegmentation
+    'BEVSegmentation': BEVSegmentation,
+    'Aligner': Aligner
 }
 
 
