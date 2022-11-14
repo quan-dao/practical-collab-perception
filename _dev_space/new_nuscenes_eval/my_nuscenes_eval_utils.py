cf156677c21f022694ac021b4beb605822f0d585
Minh-Quan DAO <minh-quan.dao@ls2n.fr>
2022-11-14 16:12:07 +0100

    init meta vehicle class

---
+++ _dev_space/new_nuscenes_eval/my_nuscenes_eval_utils.py
@@ -0,0 +1,129 @@
+from typing import List, Optional
+from nuscenes import NuScenes
+from nuscenes.eval.common.data_classes import EvalBoxes
+from nuscenes.eval.detection.data_classes import DetectionBox
+from nuscenes.utils.splits import create_splits_scenes
+import tqdm
+
+def category_to_detection_name(category_name: str) -> Optional[str]:
+    """
+    Default label mapping from nuScenes to nuScenes detection classes.
+    Note that pedestrian does not include personal_mobility, stroller and wheelchair.
+    :param category_name: Generic nuScenes class.
+    :return: nuScenes detection class.
+    """
+    # 'car' represents the meta 'vehicle' class
+    detection_mapping = {
+        'movable_object.barrier': 'ignore',
+        'vehicle.bicycle': 'car',
+        'vehicle.bus.bendy': 'car',
+        'vehicle.bus.rigid': 'car',
+        'vehicle.car': 'car',
+        'vehicle.construction': 'car',
+        'vehicle.motorcycle': 'car',
+        'human.pedestrian.adult': 'ignore',
+        'human.pedestrian.child': 'ignore',
+        'human.pedestrian.construction_worker': 'ignore',
+        'human.pedestrian.police_officer': 'ignore',
+        'movable_object.trafficcone': 'ignore',
+        'vehicle.trailer': 'car',
+        'vehicle.truck': 'car'
+    }
+
+    if category_name in detection_mapping:
+        return detection_mapping[category_name]
+    else:
+        return None
+
+
+def load_gt(nusc: NuScenes, eval_split: str, box_cls=DetectionBox, verbose: bool = False) -> EvalBoxes:
+    """
+    Loads ground truth boxes from DB.
+    :param nusc: A NuScenes instance.
+    :param eval_split: The evaluation split for which we load GT boxes.
+    :param box_cls: Type of box to load, e.g. DetectionBox or TrackingBox.
+    :param verbose: Whether to print messages to stdout.
+    :return: The GT boxes.
+    """
+    if verbose:
+        print('Loading annotations for {} split from nuScenes version: {}'.format(eval_split, nusc.version))
+    # Read out all sample_tokens in DB.
+    sample_tokens_all = [s['token'] for s in nusc.sample]
+    assert len(sample_tokens_all) > 0, "Error: Database has no samples!"
+
+    # Only keep samples from this split.
+    splits = create_splits_scenes()
+
+    # Check compatibility of split with nusc_version.
+    version = nusc.version
+    if eval_split in {'train', 'val', 'train_detect', 'train_track'}:
+        assert version.endswith('trainval'), \
+            'Error: Requested split {} which is not compatible with NuScenes version {}'.format(eval_split, version)
+    elif eval_split in {'mini_train', 'mini_val'}:
+        assert version.endswith('mini'), \
+            'Error: Requested split {} which is not compatible with NuScenes version {}'.format(eval_split, version)
+    elif eval_split == 'test':
+        assert version.endswith('test'), \
+            'Error: Requested split {} which is not compatible with NuScenes version {}'.format(eval_split, version)
+    else:
+        raise ValueError('Error: Requested split {} which this function cannot map to the correct NuScenes version.'
+                         .format(eval_split))
+
+    if eval_split == 'test':
+        # Check that you aren't trying to cheat :).
+        assert len(nusc.sample_annotation) > 0, \
+            'Error: You are trying to evaluate on the test set but you do not have the annotations!'
+
+    sample_tokens = []
+    for sample_token in sample_tokens_all:
+        scene_token = nusc.get('sample', sample_token)['scene_token']
+        scene_record = nusc.get('scene', scene_token)
+        if scene_record['name'] in splits[eval_split]:
+            sample_tokens.append(sample_token)
+
+    all_annotations = EvalBoxes()
+
+    # Load annotations and filter predictions and annotations.
+    for sample_token in tqdm.tqdm(sample_tokens, leave=verbose):
+
+        sample = nusc.get('sample', sample_token)
+        sample_annotation_tokens = sample['anns']
+
+        sample_boxes = []
+        for sample_annotation_token in sample_annotation_tokens:
+
+            sample_annotation = nusc.get('sample_annotation', sample_annotation_token)
+            if box_cls == DetectionBox:
+                # Get label name in detection task and filter unused labels.
+                detection_name = category_to_detection_name(sample_annotation['category_name'])
+                if detection_name is None or detection_name == 'ignore':
+                    continue
+
+                sample_boxes.append(
+                    box_cls(
+                        sample_token=sample_token,
+                        translation=sample_annotation['translation'],
+                        size=sample_annotation['size'],
+                        rotation=sample_annotation['rotation'],
+                        velocity=nusc.box_velocity(sample_annotation['token'])[:2],
+                        num_pts=sample_annotation['num_lidar_pts'] + sample_annotation['num_radar_pts'],
+                        detection_name=detection_name,
+                        detection_score=-1.0,  # GT samples do not have a score.
+                        attribute_name=''
+                    )
+                )
+            else:
+                raise NotImplementedError('Error: Invalid box_cls %s!' % box_cls)
+
+        all_annotations.add_boxes(sample_token, sample_boxes)
+
+    if verbose:
+        print("Loaded ground truth annotations for {} samples.".format(len(all_annotations.sample_tokens)))
+
+    return all_annotations
+
+
+if __name__ == '__main__':
+    nusc = NuScenes(version='v1.0-mini', dataroot='../../data/nuscenes/v1.0-mini', verbose=False)
+    all_annos = load_gt(nusc, 'mini_val', verbose=True)
+
