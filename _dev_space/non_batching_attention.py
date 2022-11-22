e6cb3ca30a7fac8eac4f1beb8da0c635e681eb0d
Minh-Quan DAO <minh-quan.dao@ls2n.fr>
2022-11-22 17:28:03 +0100

    add non-batching attn layer

--- _dev_space/non_batching_attention.py
+++ _dev_space/non_batching_attention.py
@@ -1,26 +1,96 @@
 import torch
+import torch.nn as nn
 import torch_scatter
 import numpy as np
+from einops import rearrange
 
 
-def single_head_attention(K: torch.Tensor, V: torch.Tensor, Q: torch.Tensor, indices_Q_to_K: torch.Tensor) -> torch.Tensor:
+def single_head_attention(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, indices_q2k: torch.Tensor) -> torch.Tensor:
     """
     Args:
-        K: (N_k, C)
-        V: (N_k, C)
-        Q: (N_q, C)
-        indices_Q_to_K: (N_k,)
+        q: (N_q, C)
+        k: (N_k, C)
+        v: (N_k, C)
+        indices_q2k: (N_k,)
+    Returns:
+        (N_q, C)
     """
-    assert K.shape[1] == V.shape[1] == Q.shape[1], f"{K.shape[1]} != {V.shape[1]} != {Q.shape[1]}"
-    n_channels = K.shape[1]
+    n_channels = k.shape[1]
 
-    Q_for_K = Q[indices_Q_to_K]  # (N_k, C)
-    attn_logits = torch.sum(Q_for_K * K, dim=1) / np.sqrt(n_channels)  # (N_k,)
+    Q_for_K = q[indices_q2k]  # (N_k, C)
+    attn_logits = torch.sum(Q_for_K * k, dim=1) / np.sqrt(n_channels)  # (N_k,)
 
     # convert attn_logits to attention weights
     exp = torch.exp(attn_logits)  # (N_k)
-    sum_exp = torch_scatter.scatter_sum(exp, indices_Q_to_K)  # (N_q)
-    attn = exp / sum_exp[indices_Q_to_K]  # (N_k,)
+    sum_exp = torch_scatter.scatter_sum(exp, indices_q2k)  # (N_q)
+    attn = exp / sum_exp[indices_q2k]  # (N_k,)
+    out = torch_scatter.scatter_sum(rearrange(attn, 'N_k -> N_k 1') * v, indices_q2k, dim=0)  # (N_q, C)
+    return out
 
-    out = torch_scatter.scatter_sum(attn * V, indices_Q_to_K, dim=0)  # (N_q, C)
-    return out  # TODO: test this
+
+def multi_head_attention(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, indices_q2k: torch.Tensor) -> torch.Tensor:
+    """
+    Args:
+        q: (head, N_q, k)
+        k: (head, N_k, k)
+        v: (head, N_k, v)
+        indices_q2k: (N_k,)
+    Returns:
+        (head, N_q, v)
+    """
+    n_ch = q.shape[-1]
+    q_for_k = q[:, indices_q2k]  # (head, N_k, k)
+    attn_logits = torch.sum(q_for_k * k, dim=2) / np.sqrt(n_ch)  # (head, N_k)
+
+    # convert attention logits to attention weight
+    exp = torch.exp(attn_logits)  # (head, N_k)
+    sum_exp = torch_scatter.scatter_sum(exp, rearrange(indices_q2k, 'N_k -> 1 N_k'), dim=1)  # (head, N_q)
+    attn = exp / sum_exp[:, indices_q2k]  # (head, N_k)
+
+    out = torch_scatter.scatter_sum(rearrange(attn, 'head N_k -> head N_k 1') * v,  # (head, N_k, v)
+                                    rearrange(indices_q2k, 'N_k -> 1 N_k'),
+                                    dim=1)  # (head, N_q, v)
+    return out
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, n_head, d_model, d_k=None, d_v=None, dropout=0.1):
+        super().__init__()
+        self.n_head = n_head
+        if d_k is None:
+            d_k = d_model // self.n_head
+            d_v = d_model // self.n_head
+        self.w_qs = nn.Linear(d_model, n_head * d_k)
+        self.w_ks = nn.Linear(d_model, n_head * d_k)
+        self.w_vs = nn.Linear(d_model, n_head * d_v)
+
+        nn.init.normal_(self.w_qs.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_k)))
+        nn.init.normal_(self.w_ks.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_k)))
+        nn.init.normal_(self.w_vs.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_v)))
+
+        self.fc = nn.Linear(n_head * d_v, d_model)
+        nn.init.xavier_normal_(self.fc.weight)
+
+        self.dropout = nn.Dropout(p=dropout)
+        self.batch_norm = nn.BatchNorm1d(d_model, eps=1e-3, momentum=0.01)
+
+    def forward(self, q, k, v, indices_q2k):
+        """
+        Args:
+            q: (N_q, d_model)
+            k: (N_k, d_model)
+            v: (N_k, d_model)
+            indices_q2k: (N_k,)
+        Returns:
+            (N_q, d_model)
+        """
+        residual = q
+
+        q = rearrange(self.w_qs(q), 'N_q (head k) -> head N_q k', head=self.n_head)
+        k = rearrange(self.w_ks(k), 'N_k (head k) -> head N_k k', head=self.n_head)
+        v = rearrange(self.w_vs(v), 'N_k (head v) -> head N_k v', head=self.n_head)
+
+        out = rearrange(multi_head_attention(q, k, v, indices_q2k), 'head N_q v -> N_q (head v)')
+        out = self.dropout(self.fc(out))
+        out = self.batch_norm(out + residual)
+        return out
