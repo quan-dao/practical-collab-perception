ee96431ba908ed3080d9817729f0cae119a1d37a
Minh-Quan DAO <minh-quan.dao@ls2n.fr>
2022-12-29 15:16:45 +0100

    validate consistency of aligner's meta based on aug_inst_idx & inst_idx

--- _dev_space/revised_test/check_aligner_head_assign_target.py
+++ _dev_space/revised_test/check_aligner_head_assign_target.py
@@ -14,9 +14,9 @@ def show_points_cls_and_boxes(batch_dict):
     show_pointcloud(points[:, 1: 4], viz_boxes(boxes[0, :, :7]), pc_colors=points_colors)
 
 
-def check_aligner_training_compute_fg(batch_dict: dict, map_point_feat2idx: dict, vehicle_class_indices: tuple):
-    print('----------show_pocheck_aligner_training_compute_fgints_cls_and_boxes----------')
-    load_data_to_tensor(batch_dict)
+def check_aligner_training_compute_fg(batch_dict: dict, map_point_feat2idx: dict, vehicle_class_indices: tuple,
+                                      return_mask_fg=False):
+    print('----------check_aligner_training_compute_fg----------')
     assert batch_dict['gt_boxes'].shape[1] == batch_dict['instances_tf'].shape[1], \
         f"{batch_dict['gt_boxes'].shape[1]} != {batch_dict['instances_tf'].shape[1]}"
     points = batch_dict['points']
@@ -36,7 +36,33 @@ def check_aligner_training_compute_fg(batch_dict: dict, map_point_feat2idx: dict
 
     points_color = torch.zeros(num_points, 3)
     points_color[mask_fg] = torch.tensor([1, 0, 0]).float()
+    if return_mask_fg:
+        return mask_fg
     show_pointcloud(points[:, 1: 4], viz_boxes(boxes[0, :, :7]), pc_colors=points_color)
+
+
+def check_consistency_2metas(batch_dict: dict, map_point_feat2idx: dict, vehicle_class_indices: tuple):
+    print('----------check_consistency_2metas----------')
+    points = batch_dict['points']
+    num_points = points.shape[0]
+    mask_fg = check_aligner_training_compute_fg(batch_dict, map_point_feat2idx, vehicle_class_indices,
+                                                return_mask_fg=True)
+    max_num_inst = batch_dict['gt_boxes'].shape[1]
+    meta = build_meta_dict(points[mask_fg], max_num_inst, map_point_feat2idx['aug_inst_idx'], map_point_feat2idx)
+
+    # ------------------------------------------------------------------
+    # compute foreground in PointAligner.assign_target
+    # ------------------------------------------------------------------
+    points_cls_idx = points[:, map_point_feat2idx['cls_idx']].long()
+    mask_fg = points.new_zeros(num_points).bool()
+    for vehicle_cls_idx in vehicle_class_indices:
+        mask_fg = torch.logical_or(mask_fg, points_cls_idx == vehicle_cls_idx)
+    target_meta = build_meta_dict(points[mask_fg], max_num_inst, map_point_feat2idx['inst_idx'], map_point_feat2idx)
+
+    assert target_meta['inst_bi'].shape[0] == meta['inst_bi'].shape[0], \
+        f"{target_meta['inst_bi'].shape[0]} == {meta['inst_bi'].shape[0]}"
+    assert torch.all(target_meta['inst_bi'] == meta['inst_bi'])
+
 
 
 if __name__ == '__main__':
@@ -44,6 +70,8 @@ if __name__ == '__main__':
     batch_size = 1
     target_batch_idx = 1
     batch_dict = make_batch_dict(is_training, batch_size, target_batch_idx)
+    load_data_to_tensor(batch_dict)
+    print('carrying out test on sample: ', batch_dict['metadata'][0]['token'])
     print_dict(batch_dict, name='batch_dict')
 
     num_pts_raw_feat = 1 + 5  # batch_idx, x, y, z, intensity, time
@@ -59,5 +87,6 @@ if __name__ == '__main__':
     vehicle_classes = ['car', 'truck', 'construction_vehicle', 'bus', 'trailer', 'motorcycle', 'bicycle']
     vehicle_class_indices = tuple([1 + class_names.index(cls_name) for cls_name in vehicle_classes])
 
-    show_points_cls_and_boxes(batch_dict)
-    check_aligner_training_compute_fg(batch_dict, map_point_feat2idx, vehicle_class_indices)
+    # show_points_cls_and_boxes(batch_dict)
+    # check_aligner_training_compute_fg(batch_dict, map_point_feat2idx, vehicle_class_indices)
+    check_consistency_2metas(batch_dict, map_point_feat2idx, vehicle_class_indices)
--- _dev_space/revised_test/utils.py
+++ _dev_space/revised_test/utils.py
@@ -6,6 +6,8 @@ from _dev_space.viz_tools import print_dict, viz_boxes
 from _dev_space.tools_box import show_pointcloud
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
+import torch_scatter
 
 
 def make_batch_dict(is_training: bool, batch_size=1, target_batch_idx=5) -> dict:
@@ -33,3 +35,65 @@ def color_points_by_detection_cls(points) -> np.ndarray:
     mask_detection_fg = points_cls_idx >= 1
     points_colors *= mask_detection_fg.reshape(-1, 1)
     return points_colors
+
+
+def build_meta_dict(fg: torch.Tensor, max_num_instances: int, index_of_instance_idx: int,
+                    map_point_feat2idx, num_sweeps=10) -> dict:
+    """
+    Args:
+        fg: (N_fg, 10) - batch_idx, x, y, z, intensity, time, sweep_idx, inst_idx, aug_idx, cls_idx
+        max_num_instances:
+        index_of_instance_idx: where is instance_index in point_feats
+    """
+    meta = {}
+
+    fg_sweep_idx = fg[:, map_point_feat2idx['sweep_idx']].long()
+    fg_batch_idx = fg[:, 0].long()
+    fg_inst_idx = fg[:, index_of_instance_idx].long()
+
+    # merge batch_idx & instance_idx
+    fg_bi_idx = fg_batch_idx * max_num_instances + fg_inst_idx  # (N,)
+
+    # merge batch_idx, instance_idx & sweep_idx
+    fg_bisw_idx = fg_bi_idx * num_sweeps + fg_sweep_idx
+
+    # group foreground points to instance
+    inst_bi, inst_bi_inv_indices = torch.unique(fg_bi_idx, sorted=True, return_inverse=True)
+    # inst_bi: (N_inst,)
+    # inst_bi_inv_indices: (N_fg,)
+
+    # group foreground points to local group
+    local_bisw, local_bisw_inv_indices = torch.unique(fg_bisw_idx, sorted=True, return_inverse=True)
+    # local_bisw: (N_local,)
+
+    meta.update({
+        'max_num_inst': max_num_instances,
+        'inst_bi': inst_bi, 'inst_bi_inv_indices': inst_bi_inv_indices,
+        'local_bisw': local_bisw, 'local_bisw_inv_indices': local_bisw_inv_indices
+    })
+
+    # -----------------------------------------------------------------------------
+    # the following is to find the center of each instance's target local group
+    # -----------------------------------------------------------------------------
+    # get the max sweep_index of each instance
+    inst_max_sweep_idx = torch_scatter.scatter_max(fg_sweep_idx, inst_bi_inv_indices)[0]  # (N_inst,)
+    # get bisw_index of each instance's max sweep
+    inst_target_bisw_idx = inst_bi * num_sweeps + inst_max_sweep_idx  # (N_inst,)
+    # for each value in inst_target_bisw_idx find WHERE (i.e., index) it appear in local_bisw
+    corr = local_bisw[:, None] == inst_target_bisw_idx[None, :]  # (N_local, N_inst)
+    corr = corr.long() * torch.arange(local_bisw.shape[0]).unsqueeze(1).to(fg.device)
+    meta['indices_local_to_inst_target'] = corr.sum(dim=0)  # (N_inst)
+
+    # -----------------------------------------------------------------------------
+    # the following is to establish correspondence between instances & locals
+    # -----------------------------------------------------------------------------
+    local_bi = local_bisw // num_sweeps
+    # for each value in local_bi find WHERE (i.e., index) it appear in inst_bi
+    local_bi_in_inst_bi = inst_bi[:, None] == local_bi[None, :]  # (N_inst, N_local)
+    local_bi_in_inst_bi = local_bi_in_inst_bi.long() * torch.arange(inst_bi.shape[0]).unsqueeze(1).to(fg.device)
+
+    meta['local_bi_in_inst_bi'] = local_bi_in_inst_bi.sum(dim=0)  # (N_local)
+    # this is identical to indices_instance_to_local which is used to
+    # broadcast inst_global_feat from (N_inst, C_inst) to (N_local, C_inst)
+
+    return meta
