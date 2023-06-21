d5b43d2130801ccd9af33aaa0195b8fd30ecc949
Minh-Quan DAO <minh-quan.dao@ls2n.fr>
2023-06-21 10:40:31 +1000

    update test cases of  late fusion

--- test_space/test_v2x_late_fusion.py
+++ test_space/test_v2x_late_fusion.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import argparse
+from nuscenes import NuScenes
 
 from pcdet.config import cfg, cfg_from_yaml_file
 from pcdet.datasets import build_dataloader
@@ -8,6 +9,8 @@ from pcdet.utils import common_utils
 from pcdet.models.detectors import build_detector
 
 from test_space.tools import to_tensor
+from workspace.o3d_visualization import PointsPainter, print_dict
+from workspace.nuscenes_temporal_utils import get_nuscenes_sensor_pose_in_global, apply_se3_, get_one_pointcloud
 
 
 def make_batch_dict(batch_size: int = 2, target_batch_idx: int = 3):
@@ -32,7 +35,8 @@ def make_batch_dict(batch_size: int = 2, target_batch_idx: int = 3):
     
     model = build_detector(cfg.MODEL, num_class=len(cfg.CLASS_NAMES), dataset=dataset)
 
-
+    nusc = dataset.nusc
+    _lidars_name = [f'LIDAR_TOP_id_{i}' for i in range(6)]
     batch_idx = 0
     for batch_dict in dataloader:
         if batch_idx != target_batch_idx:
@@ -46,6 +50,38 @@ def make_batch_dict(batch_size: int = 2, target_batch_idx: int = 3):
         with torch.no_grad():
             pred_dicts, recall_dicts = model(batch_dict)
 
+        batch_exchange_points = list()
+        for data_idx in range(len(pred_dicts)):
+            # load exchange points for visualization purpose
+            sample_token = metadata[data_idx]['lidar_token']
+            sample = nusc.get('sample', sample_token)
+            
+            target_se3_glob = np.linalg.inv(get_nuscenes_sensor_pose_in_global(nusc, sample_token))
+
+            for lidar_name, lidar_token in sample['data'].items():
+                if lidar_name not in _lidars_name:
+                    continue
+                
+                lidar_id = int(lidar_name.split('_')[-1])
+                if lidar_id == 1:
+                    continue
+                
+                glob_se3_lidar = get_nuscenes_sensor_pose_in_global(nusc, lidar_token)
+                target_se3_lidar = target_se3_glob @ glob_se3_lidar
+
+                pts = get_one_pointcloud(nusc, lidar_token)
+                apply_se3_(target_se3_lidar, points_=pts)
+                
+                # pad pts with batch_idx
+                pts = np.pad(pts, pad_width=[(0, 0), (1, 0)], constant_values=data_idx)
+
+                batch_exchange_points.append(pts)
+            
+        if len(batch_exchange_points) > 0:
+            batch_exchange_points = np.concatenate(batch_exchange_points)
+            batch_exchange_points = torch.from_numpy(batch_exchange_points).float().cuda()
+            points = torch.cat([points[:, :4], batch_exchange_points[:, :4]], dim=0)
+
         out = {
             'points': points,
             'metadata': metadata,
@@ -55,14 +91,22 @@ def make_batch_dict(batch_size: int = 2, target_batch_idx: int = 3):
         break
 
 
-def show_batch_dict():
+def show_batch_dict(chosen_batch_idx: int):
     batch_dict = torch.load('./artifact/v2x_late_fusion_pred_dict.pth', map_location=torch.device('cpu'))
 
     points = batch_dict['points']
     metadata = batch_dict['metadata']
     pred_dicts = batch_dict['pred_dicts']
-    print(f"pred_dicts: {type(pred_dicts)}")
+    
+    points = points[points[:, 0].long() == chosen_batch_idx]
+    metadata = metadata[chosen_batch_idx]
+    print("metadata['exchange']: ", metadata['exchange'])
+    pred_dicts = pred_dicts[chosen_batch_idx]
+    print_dict(pred_dicts)
+    pred_boxes = pred_dicts['pred_boxes']
 
+    painter = PointsPainter(points[:, 1: 4], pred_boxes[:, :7])
+    painter.show()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('blah')
@@ -70,9 +114,10 @@ if __name__ == '__main__':
     parser.add_argument('--target_batch_idx', type=int, default=3)
     parser.add_argument('--make_batch_dict', type=int)
     parser.add_argument('--show_batch_dict', type=int)
+    parser.add_argument('--chosen_batch_dict', type=int, default=0)
     args = parser.parse_args()
     if args.make_batch_dict == 1:
         make_batch_dict(args.batch_size, args.target_batch_idx)
     
     if args.show_batch_dict == 1:
-        show_batch_dict()
+        show_batch_dict(args.chosen_batch_dict)
