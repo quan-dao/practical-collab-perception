fb22e309d19bcc0684518428a3e24f1cdede9205
Minh-Quan DAO <minh-quan.dao@ls2n.fr>
2023-07-07 11:06:01 +1000

    to train on ronin (cont)

--- tools/train.py
+++ tools/train.py
@@ -157,7 +157,9 @@ def main():
 
     model.train()  # before wrap to DistributedDataParallel to support fixed some parameters
     if dist_train:
-        model = nn.parallel.DistributedDataParallel(model, device_ids=[idr_torch.local_rank], find_unused_parameters=True)
+        # model = nn.parallel.DistributedDataParallel(model, device_ids=[idr_torch.local_rank], find_unused_parameters=True)
+        model = nn.parallel.DistributedDataParallel(model, device_ids=[cfg.LOCAL_RANK % torch.cuda.device_count()])
+    logger.info(f'----------- Model {cfg.MODEL.NAME} created, param count: {sum([m.numel() for m in model.parameters()])} -----------')
     logger.info(model)
 
     lr_scheduler, lr_warmup_scheduler = build_scheduler(
